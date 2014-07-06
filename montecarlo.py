f5f479165bf8749ec73203d4118fc0a9d96acb1b
Giuseppe Barbalinardo <gbarbalinardo@grio.com>
2014-07-06 09:41:56 -0700

    Cleaned code

--- montecarlo.py
+++ montecarlo.py
@@ -1,12 +1,58 @@
-# -*- coding: UTF-8 -*-
 __author__ = 'giuseppe'
 import numpy as np
+import operator as op
 from math import pow
-import operator
 
 
+class Match:
+
+    def __init__(self, team1, team2, is_group):
+        self.team1 = team1
+        self.team2 = team2
+        self.is_group = is_group
+        self.winner = None
+        self.goal_scored_team1 = 0
+        self.goal_scored_team2 = 0
+        self.play_match()
+
+    def play_match(self):
+        k = 1
+        avgScoredByTeam1 = self.team1.attack / self.team2.defense * k
+        avgScoredByTeam2 = self.team2.attack / self.team1.defense * k
+        while True:
+            self.goal_scored_team1 = np.random.poisson(avgScoredByTeam1)
+            self.goal_scored_team2 = np.random.poisson(avgScoredByTeam2)
+            if self.goal_scored_team1 > self.goal_scored_team2:
+                self.team1.points += 3
+                self.team1.won += 1
+                self.team2.lost += 1
+                self.winner = self.team1
+                break
+            elif self.goal_scored_team1 < self.goal_scored_team2:
+                self.team2.points += 3
+                self.team2.won += 1
+                self.team1.lost += 1
+                self.winner = self.team2
+                break
+            else:
+                if self.is_group is True:
+                    self.team1.points += 1
+                    self.team2.points += 1
+                    self.team1.drawn += 1
+                    self.team2.drawn += 1
+                    break
+        self.team1.scored += self.goal_scored_team1
+        self.team2.scored += self.goal_scored_team2
+        self.team1.conceded += self.goal_scored_team2
+        self.team2.conceded += self.goal_scored_team1
+        self.team1.difference += self.goal_scored_team1-self.goal_scored_team2
+        self.team2.difference += self.goal_scored_team2-self.goal_scored_team1
+
+    def printMatch(self):
+        print self.team1.name + " - " + self.team2.name + " : " + str(self.goal_scored_team1) + " - " + str(self.goal_scored_team2) 
 
 class Team:
+
     def __init__(self, name, data):
         self.points = 0
         self.won = 0
@@ -24,32 +70,24 @@ class Team:
         if self.attack is None:
             print "error" + name + "is not a team"
 
-    # def __init__(self, name, attack, defense, points):
-    #     self.points = points
-    #     self.won = 0
-    #     self.lost = 0
-    #     self.drawn = 0
-    #     self.scored = 0
-    #     self.conceded = 0
-    #     self.difference = 0
-    #     self.name = name
-    #     self.attack = attack
-    #     self.defense = defense
-
     def displayTeam(self):
-        print self.name,"\t(%0.2f," %self.attack,"%0.2f)\t" %self.defense, \
-            # "\t Points: ", self.points,\
-            # "\t Goal Scored: ", self.scored, "\t Goal conceded: ", self.conceded, "\t Goal Difference: ", self.difference, \
-            # "\t Won ", self.won, "\t Lost ", self.lost, "\t Drawn ", self.drawn
+        print self.name,"\t(%0.2f," %self.attack,"%0.2f)\t" %self.defense
 
+
+# The winner of the group stage is obtained from
+# 1 - points
+# 2 - goal difference
+# 3 - goal scored
+# (no other factors considered)        
 
 class Group:
+
     def __init__(self, teams):
-        self.firstQualified = None
-        self.secondQualified = None
+        self.first_qualified = None
+        self.second_qualified = None
         self.teams = teams
         self.reset()
-        self.playFirstStage()
+        self.play_first_stage()
 
     def reset(self):
         for team in self.teams:
@@ -61,80 +99,17 @@ class Group:
             team.conceded = 0
             team.difference = 0
 
-    def playFirstStage(self):
+    def play_first_stage(self):
         for i in range(0, len(self.teams)):
             for j in range(i + 1, len(self.teams)):
                     Match(self.teams[i], self.teams[j], True)
-
-        # 1 - points
-        # 2 - goal difference
-        # 3 - goal scored
-        # (other factors not considered)
-        self.teams = sorted(self.teams, key=operator.attrgetter('points', 'difference', 'scored'))
-        self.firstQualified = self.teams[len(self.teams)-1]
-        # self.firstQualified.displayTeam()
-        # self.firstQualified
-        self.secondQualified = self.teams[len(self.teams)-2]
-        # self.secondQualified.displayTeam()
-        # print "\n"
-
-
-
-class Match:
-    def __init__(self, team1, team2, isGroup):
-        self.team1 = team1
-        self.team2 = team2
-        self.isGroup = isGroup
-        self.winner = None
-        self.goalScoredTeam1 = 0
-        self.goalScoredTeam2 = 0
-        self.playMatch()
-
-    def playMatch(self):
-        k = 1
-        avgScoredByTeam1 = self.team1.attack / self.team2.defense * k
-        avgScoredByTeam2 = self.team2.attack / self.team1.defense * k
-
-        # print "avgScoredByTeamA: " + str(avgScoredByTeam1) + " avgScoredByTeamB: " + str(avgScoredByTeam2)
-
-        while True:
-            self.goalScoredTeam1 = np.random.poisson(avgScoredByTeam1)
-            self.goalScoredTeam2 = np.random.poisson(avgScoredByTeam2)
-            if self.goalScoredTeam1 > self.goalScoredTeam2:
-                self.team1.points += 3
-                self.team1.won += 1
-                self.team2.lost += 1
-                self.winner = self.team1
-                break
-            elif self.goalScoredTeam1 < self.goalScoredTeam2:
-                self.team2.points += 3
-                self.team2.won += 1
-                self.team1.lost += 1
-                self.winner = self.team2
-                break
-                # print "The winner is ", team2.name
-            else:
-                if self.isGroup is True:
-                    self.team1.points += 1
-                    self.team2.points += 1
-                    self.team1.drawn += 1
-                    self.team2.drawn += 1
-                    break
-
-
-        self.team1.scored += self.goalScoredTeam1
-        self.team2.scored += self.goalScoredTeam2
-        self.team1.conceded += self.goalScoredTeam2
-        self.team2.conceded += self.goalScoredTeam1
-        self.team1.difference += self.goalScoredTeam1-self.goalScoredTeam2
-        self.team2.difference += self.goalScoredTeam2-self.goalScoredTeam1
-    def printMatch(self):
-        print self.team1.name + " - " + self.team2.name + " : " + str(self.goalScoredTeam1) + " - " + str(self.goalScoredTeam2) #+ (" -> " + self.winner.name if self.winner else "")
-
+        self.teams = sorted(self.teams, key=op.attrgetter('points', 'difference', 'scored'))
+        self.first_qualified = self.teams[len(self.teams)-1]
+        self.second_qualified = self.teams[len(self.teams)-2]
 
 def main():
-
-    # TEAM, ATT, DEF
+    # The teams data are obtained from FIFA statistics
+    # Team Name, Attack, Defence
     allResults = [
         ['Brazil', 1.1619160060718503, 1.3240758115969224],
         ['Croatia', 1.0, 1.1125914805779695],
@@ -203,25 +178,6 @@ def main():
     algeria = Team("ALGERIA", allResults)
     korea_republic = Team("KOREA REPUBLIC", allResults)
 
-    # SINGLE MATCH
-    brazil.displayTeam()
-    print "\t"
-    croatia.displayTeam()
-    print "\n"
-    winners = {}
-    for i in range(0, 10000):
-        match = Match(brazil, croatia, False)
-        # match.printMatch()
-        winner = match.winner
-        if winners.has_key(winner.name):
-            winners[winner.name] += 1
-        else:
-            winners[winner.name] = 1
-    for key in sorted(winners, key=winners.get, reverse=True):
-        print key + ": " + str(winners[key])
-    print "\n"
-
-    # FULL WORLD CUP
     winners = {}
     for i in range(0, 10000):
         # Play first stage
@@ -235,14 +191,14 @@ def main():
         groupH = Group([belgium, russia, algeria, korea_republic])
 
         # Play second stage
-        quarter1 = Match(groupA.firstQualified, groupB.secondQualified, False).winner
-        quarter2 = Match(groupC.firstQualified, groupD.secondQualified, False).winner
-        quarter3 = Match(groupE.firstQualified, groupF.secondQualified, False).winner
-        quarter4 = Match(groupG.firstQualified, groupH.secondQualified, False).winner
-        quarter5 = Match(groupB.firstQualified, groupA.secondQualified, False).winner
-        quarter6 = Match(groupD.firstQualified, groupC.secondQualified, False).winner
-        quarter7 = Match(groupF.firstQualified, groupE.secondQualified, False).winner
-        quarter8 = Match(groupH.firstQualified, groupG.secondQualified, False).winner
+        quarter1 = Match(groupA.first_qualified, groupB.second_qualified, False).winner
+        quarter2 = Match(groupC.first_qualified, groupD.second_qualified, False).winner
+        quarter3 = Match(groupE.first_qualified, groupF.second_qualified, False).winner
+        quarter4 = Match(groupG.first_qualified, groupH.second_qualified, False).winner
+        quarter5 = Match(groupB.first_qualified, groupA.second_qualified, False).winner
+        quarter6 = Match(groupD.first_qualified, groupC.second_qualified, False).winner
+        quarter7 = Match(groupF.first_qualified, groupE.second_qualified, False).winner
+        quarter8 = Match(groupH.first_qualified, groupG.second_qualified, False).winner
 
         # Quarters
         semifinalist1 = Match(quarter1, quarter2, False).winner
@@ -256,94 +212,14 @@ def main():
 
         # Final
         winner = Match(finalist1, finalist2, False).winner
-        # print winner.displayTeam()
 
         if winners.has_key(winner.name):
             winners[winner.name] += 1
         else:
             winners[winner.name] = 1
 
-
-    # print "Result after 12 matches"
-    # brazil.displayTeam()
-    # italy.displayTeam()
-
-    # winners = sorted(winners.iteritems(), key=operator.itemgetter(1))
     for key in sorted(winners, key=winners.get, reverse=True):
         print key + ": " + str(winners[key])
 
-    #
-    # print "\nURUGUAY WINS"
-    # for i in range(0, 10000):
-    #
-    #     italy = Team("ITALY", 1.1, 1, 3)
-    #     costa_rica = Team("COSTA RICA", .8, 1, 3)
-    #     england = Team("ENGLAND", 1.1, 1, 0)
-    #     uruguay = Team("URUGUAY", 1, 1, 3)
-    #     Match(italy, costa_rica, True)
-    #     Match(italy, uruguay, True)
-    #     Match(costa_rica, england, True)
-    #
-    #     teams = [italy, costa_rica, uruguay, england]
-    #     teams = sorted(teams, key=operator.attrgetter('points', 'difference', 'scored'))
-    #
-    #     if winners.has_key(teams[2].name):
-    #         winners[teams[2].name] += 1
-    #     else:
-    #         winners[teams[2].name] = 1
-    #
-    # for key in sorted(winners, key=winners.get, reverse=True):
-    #     print key + ": " + str(winners[key])
-    #
-    #
-    # print "\nENGLAND WINS"
-    # winners = {}
-    #
-    # for i in range(0, 10000):
-    #
-    #     italy = Team("ITALY", 1.1, 1, 3)
-    #     costa_rica = Team("COSTA RICA", .8, 1, 3)
-    #     england = Team("ENGLAND", 1.1, 1, 3)
-    #     uruguay = Team("URUGUAY", 1, 1, 0)
-    #     Match(italy, costa_rica, True)
-    #     Match(italy, uruguay, True)
-    #     Match(costa_rica, england, True)
-    #
-    #     teams = [italy, costa_rica, uruguay, england]
-    #     teams = sorted(teams, key=operator.attrgetter('points', 'difference', 'scored'))
-    #
-    #     if winners.has_key(teams[2].name):
-    #         winners[teams[2].name] += 1
-    #     else:
-    #         winners[teams[2].name] = 1
-    #
-    # for key in sorted(winners, key=winners.get, reverse=True):
-    #     print key + ": " + str(winners[key])
-    #
-    #
-    # print "\nDRAW"
-    # winners = {}
-    #
-    # for i in range(0, 10000):
-    #
-    #     italy = Team("ITALY", 1.1, 1, 3)
-    #     costa_rica = Team("COSTA RICA", .8, 1, 3)
-    #     england = Team("ENGLAND", 1.1, 1, 1)
-    #     uruguay = Team("URUGUAY", 1, 1, 1)
-    #     Match(italy, costa_rica, True)
-    #     Match(italy, uruguay, True)
-    #     Match(costa_rica, england, True)
-    #
-    #     teams = [italy, costa_rica, uruguay, england]
-    #     teams = sorted(teams, key=operator.attrgetter('points', 'difference', 'scored'))
-    #
-    #     if winners.has_key(teams[2].name):
-    #         winners[teams[2].name] += 1
-    #     else:
-    #         winners[teams[2].name] = 1
-    #
-    # for key in sorted(winners, key=winners.get, reverse=True):
-    #     print key + ": " + str(winners[key])
-    #
 if __name__ == '__main__':
     main()
